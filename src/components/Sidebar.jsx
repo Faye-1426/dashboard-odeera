f3f8be34a551a87b72b47cd349d286b46ff8e7ec
aryadira <aryadyra27@gmail.com>
2023-11-15 10:49:52 +0700

    layout added

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,13 +2,13 @@ import { Link } from "react-router-dom";
 
 const Sidebar = () => {
   return (
-    <div className='sidebar h-screen bg-neutral-100 sticky top-0 w-max'>
+    <aside className='sidebar h-screen bg-neutral-100 sticky top-0 w-max p-5'>
       <ul className='navlist'>
         <Link to='/dashboard'>Dasboard</Link>
         <Link to='/education'>Education</Link>
         <Link to='/report'>Report</Link>
       </ul>
-    </div>
+    </aside>
   );
 };
 
